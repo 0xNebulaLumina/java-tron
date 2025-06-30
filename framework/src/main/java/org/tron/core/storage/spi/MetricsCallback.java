ca80e81573466a77dbc1d4f61f68de811474f60d
0xNebulaLumina <0xNebulaLumina@proton.me>
2025-06-30 08:58:32 +0200

    format codes

--- framework/src/main/java/org/tron/core/storage/spi/MetricsCallback.java
+++ framework/src/main/java/org/tron/core/storage/spi/MetricsCallback.java
@@ -2,9 +2,7 @@ package org.tron.core.storage.spi;
 
 import java.util.Map;
 
-/**
- * Callback interface for receiving storage metrics.
- */
+/** Callback interface for receiving storage metrics. */
 public interface MetricsCallback {
-    void onMetrics(String dbName, Map<String, Object> metrics);
-} 
+  void onMetrics(String dbName, Map<String, Object> metrics);
+}
--- framework/src/main/java/org/tron/core/storage/spi/StorageStats.java
+++ framework/src/main/java/org/tron/core/storage/spi/StorageStats.java
@@ -3,68 +3,71 @@ package org.tron.core.storage.spi;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Statistics information for a storage database.
- */
+/** Statistics information for a storage database. */
 public class StorageStats {
-    private long totalKeys;
-    private long totalSize;
-    private Map<String, String> engineStats = new HashMap<>();
-    private long lastModified;
+  private long totalKeys;
+  private long totalSize;
+  private Map<String, String> engineStats = new HashMap<>();
+  private long lastModified;
 
-    public StorageStats() {}
+  public StorageStats() {}
 
-    public StorageStats(long totalKeys, long totalSize, Map<String, String> engineStats, long lastModified) {
-        this.totalKeys = totalKeys;
-        this.totalSize = totalSize;
-        this.engineStats = engineStats != null ? engineStats : new HashMap<>();
-        this.lastModified = lastModified;
-    }
+  public StorageStats(
+      long totalKeys, long totalSize, Map<String, String> engineStats, long lastModified) {
+    this.totalKeys = totalKeys;
+    this.totalSize = totalSize;
+    this.engineStats = engineStats != null ? engineStats : new HashMap<>();
+    this.lastModified = lastModified;
+  }
 
-    // Getters and setters
-    public long getTotalKeys() {
-        return totalKeys;
-    }
+  // Getters and setters
+  public long getTotalKeys() {
+    return totalKeys;
+  }
 
-    public void setTotalKeys(long totalKeys) {
-        this.totalKeys = totalKeys;
-    }
+  public void setTotalKeys(long totalKeys) {
+    this.totalKeys = totalKeys;
+  }
 
-    public long getTotalSize() {
-        return totalSize;
-    }
+  public long getTotalSize() {
+    return totalSize;
+  }
 
-    public void setTotalSize(long totalSize) {
-        this.totalSize = totalSize;
-    }
+  public void setTotalSize(long totalSize) {
+    this.totalSize = totalSize;
+  }
 
-    public Map<String, String> getEngineStats() {
-        return engineStats;
-    }
+  public Map<String, String> getEngineStats() {
+    return engineStats;
+  }
 
-    public void setEngineStats(Map<String, String> engineStats) {
-        this.engineStats = engineStats;
-    }
+  public void setEngineStats(Map<String, String> engineStats) {
+    this.engineStats = engineStats;
+  }
 
-    public void addEngineStat(String key, String value) {
-        this.engineStats.put(key, value);
-    }
+  public void addEngineStat(String key, String value) {
+    this.engineStats.put(key, value);
+  }
 
-    public long getLastModified() {
-        return lastModified;
-    }
+  public long getLastModified() {
+    return lastModified;
+  }
 
-    public void setLastModified(long lastModified) {
-        this.lastModified = lastModified;
-    }
+  public void setLastModified(long lastModified) {
+    this.lastModified = lastModified;
+  }
 
-    @Override
-    public String toString() {
-        return "StorageStats{" +
-                "totalKeys=" + totalKeys +
-                ", totalSize=" + totalSize +
-                ", engineStats=" + engineStats +
-                ", lastModified=" + lastModified +
-                '}';
-    }
-} 
+  @Override
+  public String toString() {
+    return "StorageStats{"
+        + "totalKeys="
+        + totalKeys
+        + ", totalSize="
+        + totalSize
+        + ", engineStats="
+        + engineStats
+        + ", lastModified="
+        + lastModified
+        + '}';
+  }
+}
